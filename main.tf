6d27be1323ff4c844382c0957180e2e29359bd61
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-03 11:35:40 +0530

    peering

--- main.tf
+++ main.tf
@@ -3,6 +3,4 @@ module "vpc" {
   vpc = var.vpc
   env = var.env
   management_vpc = var.management_vpc
-  private_subnets           = each.value.private_subnets
-  public_subnets            = each.value.public_subnets
 }
