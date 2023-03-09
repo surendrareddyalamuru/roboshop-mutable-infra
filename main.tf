55c85f0e78cdda46bc8e987fb07de56001231e18
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-09 11:11:05 +0530

    outputs

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ module "vpc" {
   env = var.env
   management_vpc = var.management_vpc
 }
-
-module "docdb" {
-  source = "github.com/surendrareddyalamuru/tf-module-docdb"
-  docdb = var.docdb
-  env = var.env
-  subnets = local.database_private_subnets[0].id
-}
-
-
-#output "app_private_subnets" {
-#  value = local.app_private_subnets[0].id
+#
+#module "docdb" {
+#  source = "github.com/surendrareddyalamuru/tf-module-docdb"
+#  docdb = var.docdb
+#  env = var.env
+#  subnets = local.database_private_subnets[0].id
 #}
+
+
+output "database_private_subnets" {
+  value = local.database_private_subnets
+}
