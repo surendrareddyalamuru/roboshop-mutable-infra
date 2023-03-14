0abeeb84b05b503173c9f5f5172cc17ac06d6050
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-14 10:43:34 +0530

    outputs

--- main.tf
+++ main.tf
@@ -32,6 +32,6 @@ module "vpc" {
 #  value = local.database_private_subnets[*].id
 #}
 
-output "subnets" {
-  value = lookup(module.vpc, "private_subnets", null)
-}
+#output "subnets" {
+#  value = lookup(module.vpc, "private_subnets", null)
+#}
