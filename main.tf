6a8c2ea5804dbee3070ae3cb397394c4e18101c5
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-14 10:51:56 +0530

    outputs

--- main.tf
+++ main.tf
@@ -33,5 +33,5 @@ module "vpc" {
 #}
 
 output "app_subnets" {
-  value = [for i, j in module.vpc : j.private_subnets["app"]["subnets"]]
+  value = [for i, j in module.vpc : j.private_subnets["app"]["subnets"][*].id]
 }
